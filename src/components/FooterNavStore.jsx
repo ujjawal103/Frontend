75c4ba19462d8bc0a86171ef902d05f23c25a565
ujjawal103 <ujjawaljaiswal618@gmail.com>
2025-12-20 01:04:32 +0530

    footer bar styling change

--- src/components/FooterNavStore.jsx
+++ src/components/FooterNavStore.jsx
@@ -56,22 +56,254 @@ export default function FooterNavStore() {
 
   const isActive = (path) => location.pathname === path;
 
-  return (
+  // return (
+  //   <>
+  //     {loading && <Loading message={message} />}
+
+  //     <div className="fixed bottom-0 left-0 w-full md:top-0 md:left-0 md:h-full md:w-60 border-t shadow-md flex md:flex-col justify-between items-center py-2 z-50 bg-gray-900">
+  //       <div className="flex md:flex-col items-center justify-between w-[80vw] md:w-full">
+
+  //         {/* Home */}
+  //         <div
+  //           className={`md:w-60 cursor-pointer ${
+  //             isActive("/store-home") ? "bg-gray-800" : "hover:bg-gray-800"
+  //           }`}
+  //         >
+  //           <Link
+  //             to="/store-home"
+  //             className="flex flex-col items-center text-white w-auto md:flex-row md:items-center md:gap-2 p-4"
+  //           >
+  //             <FaHome size={22} />
+  //             <span className="text-xs md:text-sm">Home</span>
+  //           </Link>
+  //         </div>
+
+  //         {/* Menu Items */}
+  //         <div
+  //           className={`md:w-60 cursor-pointer ${
+  //             isActive("/items") ? "bg-gray-800" : "hover:bg-gray-800"
+  //           }`}
+  //         >
+  //           <Link
+  //             to="/items"
+  //             className="flex flex-col items-center text-white w-auto md:flex-row md:items-center md:gap-2 p-4"
+  //           >
+  //             <FaUtensils size={22} />
+  //             <span className="text-xs md:text-sm">Menu Items</span>
+  //           </Link>
+  //         </div>
+
+  //         {/* Tables */}
+  //         <div
+  //           className={`md:w-60 cursor-pointer ${
+  //             isActive("/table-management")
+  //               ? "bg-gray-800"
+  //               : "hover:bg-gray-800"
+  //           }`}
+  //         >
+  //           <Link
+  //             to="/table-management"
+  //             className="flex flex-col items-center text-white w-auto md:flex-row md:items-center md:gap-2 p-4"
+  //           >
+  //             <FaChair size={22} />
+  //             <span className="text-xs md:text-sm">Tables</span>
+  //           </Link>
+  //         </div>
+
+
+
+  //         {/* Create Order */}
+  //         <div
+  //           className={`hidden md:w-60 md:flex cursor-pointer ${
+  //             isActive("/store-menu") ? "bg-gray-800" : "hover:bg-gray-800"
+  //           }`}
+  //         >
+  //           <Link
+  //             to="/store-menu"
+  //             className="flex items-center gap-2 text-white w-full p-4"
+  //           >
+  //             <FaPlusCircle size={22} />
+  //             <span className="text-sm">Create Order</span>
+  //           </Link>
+  //         </div>
+
+
+
+  //         {/* Today Orders */}
+  //         <div
+  //           className={`hidden md:w-60 md:flex cursor-pointer ${
+  //             isActive("/today-orders") ? "bg-gray-800" : "hover:bg-gray-800"
+  //           }`}
+  //         >
+  //           <Link
+  //             to="/today-orders"
+  //             className="flex items-center gap-2 text-white w-full p-4"
+  //           >
+  //             <FaClipboardList size={22} />
+  //             <span className="text-sm">Today Orders</span>
+  //           </Link>
+  //         </div>
+
+
+
+
+  //         {/* Orders */}
+  //         <div
+  //           className={`md:w-60 cursor-pointer ${
+  //             isActive("/store-orders") ? "bg-gray-800" : "hover:bg-gray-800"
+  //           }`}
+  //         >
+  //           <Link
+  //             to="/store-orders"
+  //             className="flex flex-col items-center text-white w-auto md:flex-row md:items-center md:gap-2 p-4"
+  //           >
+  //             <FaClock size={22} />
+  //             <span className="text-xs md:text-sm">Orders</span>
+  //           </Link>
+  //         </div>
+
+  //         {/* Pending Orders */}
+  //         {/* <div
+  //           className={`hidden md:w-60 md:flex cursor-pointer ${
+  //             isActive("/store-bookings") ? "bg-gray-800" : "hover:bg-gray-800"
+  //           }`}
+  //         >
+  //           <Link
+  //             to="/store-bookings"
+  //             className="flex items-center gap-2 text-white w-full p-4"
+  //           >
+  //             <FaCalendarCheck size={22} />
+  //             <span className="text-sm">Pending Orders</span>
+  //           </Link>
+  //         </div> */}
+
+
+  //         {/* Table Orders */}
+  //         <div
+  //           className={`hidden md:w-60 md:flex cursor-pointer ${
+  //             isActive("/table-analytics")
+  //               ? "bg-gray-800"
+  //               : "hover:bg-gray-800"
+  //           }`}
+  //         >
+  //           <Link
+  //             to="/table-analytics"
+  //             className="flex items-center gap-2 text-white w-full p-4"
+  //           >
+  //             <FaTable size={22} />
+  //             <span className="text-sm">Table Orders</span>
+  //           </Link>
+  //         </div>
+
+          
+
+          
+
+  //         {/* Daily Analytics */}
+  //         <div
+  //           className={`hidden md:w-60 md:flex cursor-pointer ${
+  //             isActive("/daily-analytics")
+  //               ? "bg-gray-800"
+  //               : "hover:bg-gray-800"
+  //           }`}
+  //         >
+  //           <Link
+  //             to="/daily-analytics"
+  //             className="flex items-center gap-2 text-white w-full p-4"
+  //           >
+  //             <FaChartBar size={22} />
+  //             <span className="text-sm">Daily Analytics</span>
+  //           </Link>
+  //         </div>
+
+  //         {/* Monthly Analytics */}
+  //         <div
+  //           className={`hidden md:w-60 md:flex cursor-pointer ${
+  //             isActive("/monthly-analytics")
+  //               ? "bg-gray-800"
+  //               : "hover:bg-gray-800"
+  //           }`}
+  //         >
+  //           <Link
+  //             to="/monthly-analytics"
+  //             className="flex items-center gap-2 text-white w-full p-4"
+  //           >
+  //             <FaChartLine size={22} />
+  //             <span className="text-sm">Monthly Analytics</span>
+  //           </Link>
+  //         </div>
+
+
+  //         <div
+  //           className={`hidden md:w-60 md:flex cursor-pointer ${
+  //             isActive("/gst-charges")
+  //               ? "bg-gray-800"
+  //               : "hover:bg-gray-800"
+  //           }`}
+  //         >
+  //           <Link
+  //             to="/gst-charges"
+  //             className="flex items-center gap-2 text-white w-full p-4"
+  //           >
+  //             <FaFileInvoiceDollar size={22} />
+  //             <span className="text-sm">GST & Charges</span>
+  //           </Link>
+  //         </div>
+
+          
+  //       </div>
+
+  //       {/* Account */}
+  //       <div
+  //         className={`md:w-60 p-4 cursor-pointer ${
+  //           drawerOpen ? "bg-gray-800" : "hover:bg-gray-800"
+  //         }`}
+  //         onClick={handleProfile}
+  //       >
+  //         <button className="flex flex-col items-center text-white w-auto md:flex-row md:items-center md:gap-2">
+  //           <FaUser size={22} />
+  //           <span className="text-xs md:text-sm">Account</span>
+  //         </button>
+  //       </div>
+
+  //       {/* Drawer */}
+  //       {profileFetched?.store && (
+  //         <AccountDrawerStore
+  //           isOpen={drawerOpen}
+  //           onClose={() => setDrawerOpen(false)}
+  //           store={profileFetched.store}
+  //         />
+  //       )}
+  //     </div>
+  //   </>
+  // );
+
+
+
+
+
+
+
+
+
+ return (
     <>
       {loading && <Loading message={message} />}
 
-      <div className="fixed bottom-0 left-0 w-full md:top-0 md:left-0 md:h-full md:w-60 border-t shadow-md flex md:flex-col justify-between items-center py-2 z-50 bg-gray-900">
+      <div className="fixed bottom-0 left-0 w-full md:top-0 md:left-0 md:h-full md:w-60 md:border-t shadow-md flex md:flex-col justify-between items-center py-2 z-50 bg-white md:bg-gray-900">
         <div className="flex md:flex-col items-center justify-between w-[80vw] md:w-full">
 
           {/* Home */}
           <div
             className={`md:w-60 cursor-pointer ${
-              isActive("/store-home") ? "bg-gray-800" : "hover:bg-gray-800"
+              isActive("/store-home") ? "md:bg-gray-800" : "md:hover:bg-gray-800"
             }`}
           >
             <Link
               to="/store-home"
-              className="flex flex-col items-center text-white w-auto md:flex-row md:items-center md:gap-2 p-4"
+              className={`flex flex-col items-center text-black md:text-white ${
+              isActive("/store-home") ? "text-pink-700 border-t" : "sm:hover:pink-600"
+            }  w-auto md:flex-row md:items-center md:gap-2 p-4`}
             >
               <FaHome size={22} />
               <span className="text-xs md:text-sm">Home</span>
@@ -81,12 +313,14 @@ export default function FooterNavStore() {
           {/* Menu Items */}
           <div
             className={`md:w-60 cursor-pointer ${
-              isActive("/items") ? "bg-gray-800" : "hover:bg-gray-800"
+              isActive("/items") ? "md:bg-gray-800" : "md:hover:bg-gray-800"
             }`}
           >
             <Link
               to="/items"
-              className="flex flex-col items-center text-white w-auto md:flex-row md:items-center md:gap-2 p-4"
+              className={`flex flex-col items-center text-black md:text-white ${
+              isActive("/items") ? "text-pink-700 border-t" : "sm:hover:pink-600"
+            }  w-auto md:flex-row md:items-center md:gap-2 p-4`}
             >
               <FaUtensils size={22} />
               <span className="text-xs md:text-sm">Menu Items</span>
@@ -97,13 +331,14 @@ export default function FooterNavStore() {
           <div
             className={`md:w-60 cursor-pointer ${
               isActive("/table-management")
-                ? "bg-gray-800"
-                : "hover:bg-gray-800"
+                ? "md:bg-gray-800" : "md:hover:bg-gray-800"
             }`}
           >
             <Link
               to="/table-management"
-              className="flex flex-col items-center text-white w-auto md:flex-row md:items-center md:gap-2 p-4"
+               className={`flex flex-col items-center text-black md:text-white ${
+              isActive("/table-management") ? "text-pink-700 border-t" : "sm:hover:pink-600"
+            }  w-auto md:flex-row md:items-center md:gap-2 p-4`}
             >
               <FaChair size={22} />
               <span className="text-xs md:text-sm">Tables</span>
@@ -115,12 +350,12 @@ export default function FooterNavStore() {
           {/* Create Order */}
           <div
             className={`hidden md:w-60 md:flex cursor-pointer ${
-              isActive("/store-menu") ? "bg-gray-800" : "hover:bg-gray-800"
+              isActive("/store-menu") ? "md:bg-gray-800" : "md:hover:bg-gray-800"
             }`}
           >
             <Link
               to="/store-menu"
-              className="flex items-center gap-2 text-white w-full p-4"
+              className="flex items-center gap-2 text-black md:text-white w-full p-4"
             >
               <FaPlusCircle size={22} />
               <span className="text-sm">Create Order</span>
@@ -132,12 +367,12 @@ export default function FooterNavStore() {
           {/* Today Orders */}
           <div
             className={`hidden md:w-60 md:flex cursor-pointer ${
-              isActive("/today-orders") ? "bg-gray-800" : "hover:bg-gray-800"
+              isActive("/today-orders") ? "md:bg-gray-800" : "md:hover:bg-gray-800"
             }`}
           >
             <Link
               to="/today-orders"
-              className="flex items-center gap-2 text-white w-full p-4"
+              className="flex items-center gap-2 text-black md:text-white w-full p-4"
             >
               <FaClipboardList size={22} />
               <span className="text-sm">Today Orders</span>
@@ -150,12 +385,14 @@ export default function FooterNavStore() {
           {/* Orders */}
           <div
             className={`md:w-60 cursor-pointer ${
-              isActive("/store-orders") ? "bg-gray-800" : "hover:bg-gray-800"
+              isActive("/store-orders") ? "md:bg-gray-800" : "md:hover:bg-gray-800"
             }`}
           >
             <Link
               to="/store-orders"
-              className="flex flex-col items-center text-white w-auto md:flex-row md:items-center md:gap-2 p-4"
+               className={`flex flex-col items-center text-black md:text-white ${
+              isActive("/store-orders") ? "text-pink-700 border-t" : "sm:hover:pink-600"
+            }  w-auto md:flex-row md:items-center md:gap-2 p-4`}
             >
               <FaClock size={22} />
               <span className="text-xs md:text-sm">Orders</span>
@@ -182,13 +419,12 @@ export default function FooterNavStore() {
           <div
             className={`hidden md:w-60 md:flex cursor-pointer ${
               isActive("/table-analytics")
-                ? "bg-gray-800"
-                : "hover:bg-gray-800"
+                ? "md:bg-gray-800" : "md:hover:bg-gray-800"
             }`}
           >
             <Link
               to="/table-analytics"
-              className="flex items-center gap-2 text-white w-full p-4"
+              className="flex items-center gap-2 text-black md:text-white w-full p-4"
             >
               <FaTable size={22} />
               <span className="text-sm">Table Orders</span>
@@ -203,13 +439,12 @@ export default function FooterNavStore() {
           <div
             className={`hidden md:w-60 md:flex cursor-pointer ${
               isActive("/daily-analytics")
-                ? "bg-gray-800"
-                : "hover:bg-gray-800"
+                ? "md:bg-gray-800" : "md:hover:bg-gray-800"
             }`}
           >
             <Link
               to="/daily-analytics"
-              className="flex items-center gap-2 text-white w-full p-4"
+              className="flex items-center gap-2 text-black md:text-white w-full p-4"
             >
               <FaChartBar size={22} />
               <span className="text-sm">Daily Analytics</span>
@@ -220,13 +455,12 @@ export default function FooterNavStore() {
           <div
             className={`hidden md:w-60 md:flex cursor-pointer ${
               isActive("/monthly-analytics")
-                ? "bg-gray-800"
-                : "hover:bg-gray-800"
+                ? "md:bg-gray-800" : "md:hover:bg-gray-800"
             }`}
           >
             <Link
               to="/monthly-analytics"
-              className="flex items-center gap-2 text-white w-full p-4"
+              className="flex items-center gap-2 text-black md:text-white w-full p-4"
             >
               <FaChartLine size={22} />
               <span className="text-sm">Monthly Analytics</span>
@@ -237,13 +471,12 @@ export default function FooterNavStore() {
           <div
             className={`hidden md:w-60 md:flex cursor-pointer ${
               isActive("/gst-charges")
-                ? "bg-gray-800"
-                : "hover:bg-gray-800"
+                ? "md:bg-gray-800" : "md:hover:bg-gray-800"
             }`}
           >
             <Link
               to="/gst-charges"
-              className="flex items-center gap-2 text-white w-full p-4"
+              className="flex items-center gap-2 text-black md:text-white w-full p-4"
             >
               <FaFileInvoiceDollar size={22} />
               <span className="text-sm">GST & Charges</span>
@@ -256,25 +489,29 @@ export default function FooterNavStore() {
         {/* Account */}
         <div
           className={`md:w-60 p-4 cursor-pointer ${
-            drawerOpen ? "bg-gray-800" : "hover:bg-gray-800"
+            drawerOpen ? "md:bg-gray-800" : "md:hover:bg-gray-800"
           }`}
           onClick={handleProfile}
         >
-          <button className="flex flex-col items-center text-white w-auto md:flex-row md:items-center md:gap-2">
+          <button className={`flex flex-col items-center text-black md:text-white ${
+              drawerOpen ? "text-pink-700 border-t" : "sm:hover:pink-600"
+            }  w-auto md:flex-row md:items-center md:gap-2`}>
             <FaUser size={22} />
             <span className="text-xs md:text-sm">Account</span>
           </button>
         </div>
 
         {/* Drawer */}
-        {profileFetched?.store && (
+        {(profileFetched || drawerOpen) && (
           <AccountDrawerStore
             isOpen={drawerOpen}
             onClose={() => setDrawerOpen(false)}
-            store={profileFetched.store}
+            store={profileFetched}
           />
         )}
       </div>
     </>
   );
+
+
 }
