b2c33b0ad93493c92f9fc56fa8fe23f36f8b6986
ujjawal103 <ujjawaljaiswal618@gmail.com>
2026-03-30 20:14:18 +0530

    select delivery page ui

--- src/pages/SelectLocationPage.jsx
+++ src/pages/SelectLocationPage.jsx
@@ -464,7 +464,7 @@ useEffect(() => {
       {/* MAP */}
       <div
         ref={mapRef}
-        className="flex-1  relative mb-[120px]"
+        className="flex-1 relative"
       />
 
       {/* CENTER PIN */}
@@ -488,7 +488,7 @@ useEffect(() => {
       </div>
 
       {/* CURRENT LOCATION BUTTON */}
-        <div className="absolute bottom-[120px] left-1/2 -translate-x-1/2 z-10">
+        <div className="absolute bottom-38 left-1/2 -translate-x-1/2 z-10">
           <button
             onClick={useCurrentLocation}
             disabled={detectingCurrentLocation}
